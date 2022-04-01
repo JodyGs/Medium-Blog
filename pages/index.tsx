6a9a574257c4c6a3cf6463c23811f5761e57e588
JodyGs <jody.gonzales@webingenierie.dev>
2022-04-01 21:20:48 +0200

    Home - Post Section

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Header from '../components/Header'
 import Image from 'next/image'
 import mediumMLogo from '../public/assets/Medium-M-logo.png'
 import {sanityClient, urlFor} from '../sanity'
+import Link from 'next/link'
 
 interface Props {
   posts: [Post]
@@ -38,6 +39,24 @@ const Home: NextPage = ({posts}: Props) => {
           src="/assets/Medium-M-logo.png"
           alt="Medium Logo"
         />
+      </div>
+
+      {/* Posts */}
+      <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 md:p-6'>
+        {posts.map(post => (
+          <Link key={post._id} href={`/post/${post.slug.current}`}>
+            <div className='border rounded-lg group cursor-pointer overflow-hidden'>
+              <img className='h-60 w-full object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out' src={urlFor(post.mainImage).url()!} alt={`Article: ${post.title}`} />
+              <div className='flex justify-between p-5 bg-white'>
+                <div>
+                  <p className='text-lg font-bold' >{post.title}</p>
+                  <p className='text-xs'>{post.description} by {post.author.name}</p>
+                </div>
+                <img className='h-12 w-12 rounded-full object-cover' src={urlFor(post.author.image).url()!} alt={`${post.author.name} photography`} />
+              </div>
+            </div>
+          </Link>
+        ))}
       </div>
     </div>
   )
